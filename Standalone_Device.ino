63933669e0d32f7735262ead03812f6a9cbbfd8f
Vaibhav <43792238+vaibhavm99@users.noreply.github.com>
2020-11-20 22:00:51 +0530

    Update Standalone_Device.ino

--- Standalone_Device.ino
+++ Standalone_Device.ino
@@ -1,19 +1,41 @@
 #include <SoftwareSerial.h>
+#include <TinyGPS.h>
 
-SoftwareSerial mySerial(9, 10);    // RX | TX
-int button = 2;
+int state = 0;
+float gpslat, gpslon;
+
+TinyGPS gps;
+SoftwareSerial sgps(4, 5);  //  RX | TX
+SoftwareSerial sgsm(2, 3);   // RX | TX
+
+int button = 6;
 int flag = 0;
 void setup()
 {
-  mySerial.begin(9600);   // Setting the baud rate of GSM Module  
+  sgsm.begin(9600);
+  sgps.begin(9600);
   pinMode(button, INPUT);
 }
 
 
 void loop()
 {
+  // Get GPS Location 
+  sgps.listen();
+  while (sgps.available())
+  {
+    int c = sgps.read();
+    if (gps.encode(c))
+    {
+      gps.f_get_position(&gpslat, &gpslon);
+    }
+  }
+
+  // Increase button counter
   if(digitalRead(button) == HIGH)
     flag++;
+  
+  
   if(flag == 3)     //If button pressed 3 times
   {
     flag = 0;
@@ -21,22 +43,30 @@ void loop()
   }
      
 delay(100);
-
-
- if (mySerial.available()>0)             //Read any data coming from GSM Module
-   Serial.write(mySerial.read());
 }
 
 
  void SendMessage()
 {
-  mySerial.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
-  delay(1000);  // Delay of 1000 milli seconds or 1 second
-  mySerial.println("AT+CMGS=\"+91xxxxxxxxxx\"\r"); // Replace x with mobile number
-  delay(1000);
-  mySerial.println("I need Help! My location is");// The SMS text you want to send
-  delay(100);
-   mySerial.println((char)26);// ASCII code of CTRL+Z
-  delay(1000);
+      sgsm.listen();
+      sgsm.print("\r");
+      delay(1000);
+      sgsm.print("AT+CMGF=1\r");
+      delay(1000);
+      sgsm.print("AT+CMGS=\"+91XXXXXXXXXX\"\r");
+      delay(1000);
+      //The text of the message to be sent.
+   
+      sgsm.println("I need HELP! My location is ");
+      sgsm.print("https://www.google.com/maps/?q=");
+      sgsm.print(gpslat, 6);
+      sgsm.print(",");
+      sgsm.print(gpslon, 6);
+   
+   
+   delay(1000);
+      sgsm.write(0x1A);
+      delay(1000);
+      state = 1;
 }
 
