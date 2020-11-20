22fff5ffd3b006d91c1f7b71decff62d5595dcc4
Vaibhav <43792238+vaibhavm99@users.noreply.github.com>
2020-11-20 21:43:21 +0530

    Update Standalone_Device.ino

--- Standalone_Device.ino
+++ Standalone_Device.ino
@@ -2,6 +2,7 @@
 
 SoftwareSerial mySerial(9, 10);    // RX | TX
 int button = 2;
+int flag = 0;
 void setup()
 {
   mySerial.begin(9600);   // Setting the baud rate of GSM Module  
@@ -11,7 +12,16 @@ void setup()
 
 void loop()
 {
-  
+  if(digitalRead(button) == HIGH)
+    flag++;
+  if(flag == 3)     //If button pressed 3 times
+  {
+    flag = 0;
+    SendMessage();          
+  }
+     
+delay(100);
+
 
  if (mySerial.available()>0)             //Read any data coming from GSM Module
    Serial.write(mySerial.read());
@@ -24,7 +34,7 @@ void loop()
   delay(1000);  // Delay of 1000 milli seconds or 1 second
   mySerial.println("AT+CMGS=\"+91xxxxxxxxxx\"\r"); // Replace x with mobile number
   delay(1000);
-  mySerial.println("I am SMS from GSM Module");// The SMS text you want to send
+  mySerial.println("I need Help! My location is");// The SMS text you want to send
   delay(100);
    mySerial.println((char)26);// ASCII code of CTRL+Z
   delay(1000);
