f9eb20b18020c4922b61105b4fde5c6afaff3a8f
Vaibhav <43792238+vaibhavm99@users.noreply.github.com>
2020-11-20 22:08:07 +0530

    Update Device_with_app.ino

--- Device_with_app.ino
+++ Device_with_app.ino
@@ -7,10 +7,17 @@ void setup() {
   pinMode(button, INPUT);
   BTserial.begin(9600); 
 }
-
+float s = millis();
 void loop() {
   if(digitalRead(button) == HIGH)
+  {
     flag++;
+    if((e-s)>5000)
+    {
+      flag = 0;
+      s = millis();
+    }
+  }
   if(flag == 3)     //If button pressed 3 times
   {
     flag = 0;
